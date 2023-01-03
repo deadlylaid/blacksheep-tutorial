6efcb09011f9ede85e400953d772015adeeb67b2
deadlylaid <deadlylaid@gmail.com>
2023-01-04 00:00:43 +0900

    type hint 추가

--- apps/main.py
+++ apps/main.py
@@ -1,6 +1,13 @@
 from blacksheep import Application
+from typing import Dict, List, TypedDict
 
 app = Application()
+
+
+class PostTypedDict(TypedDict):
+    post_id: int
+    name: str
+    url: str
 
 
 @app.router.get("/")
@@ -12,13 +19,18 @@ posts_app = Application()
 
 
 @posts_app.router.get("/")
-def posts() -> dict:
+def posts() -> Dict[str, List[PostTypedDict]]:
     return {
         'posts': [
             {'post_id': 1, 'name': 'post_1', 'url': '/posts/image1.jpg'},
             {'post_id': 2, 'name': 'post_2', 'url': '/posts/image2.jpg'}
         ]
     }
+
+
+@posts_app.router.get("/1")
+def posts_detail() -> PostTypedDict:
+    return {'post_id': 1, 'name': 'post_1', 'url': '/posts/image1.jpg'}
 
 
 app.mount_registry.auto_events = True
